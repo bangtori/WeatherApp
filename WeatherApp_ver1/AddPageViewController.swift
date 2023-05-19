96631aa2a353c162d8408b75c3e57375f4ea445a
Tori <ttang1135@naver.com>
2023-05-19 15:00:54 +0900

    feat : Add Page 도시 추가 기능
    도시이름 UserDefaults저장 및 테이블 뷰로 불러오기

--- WeatherApp_ver1/AddPageViewController.swift
+++ WeatherApp_ver1/AddPageViewController.swift
@@ -11,29 +11,62 @@ class AddPageViewController: UIViewController {
     
     @IBOutlet var tableView: UITableView!
     @IBOutlet weak var cityInputTextField: UITextField!
+    let userDefaults = UserDefaults.standard
+    var weatherManager = WeatherManager()
     
-    var city:[String] = ["Seoul"]
+    var city:[String] = []{
+        didSet{
+            self.saveData()
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.delegate = self
         self.tableView.dataSource = self
-        
-
-        // Do any additional setup after loading the view.
+        weatherManager.delegate = self
+        loadData()
     }
     @IBAction func addBtnClicked(_ sender: UIButton) {
+        guard let searchCity = cityInputTextField.text else { return }
+        weatherManager.fetchWeather(cityName: searchCity)
     }
-    
+    private func loadData(){
+        guard let data = userDefaults.stringArray(forKey: "city") else { return }
+        city = data
+    }
+    private func saveData(){
+        userDefaults.set(city, forKey: "city")
+        tableView.reloadData()
+    }
 }
 extension AddPageViewController : UITableViewDelegate, UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return city.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        print("dd")
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
 
         cell.textLabel?.text = city[indexPath.row]
         return cell
+    }
+}
+
+extension AddPageViewController : WeatherManagerDelegate{
+    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherInfo) {
+        //결과값이 있다면 데이터 저장
+        let data = weather.name
+        city.append(data)
+        cityInputTextField.text = ""
+    }
+    
+    func didFailWithError(error: ErrorMessage) {
+        DispatchQueue.main.async {
+            self.showAlert(message: error.message)
+        }
+    }
+    func showAlert(message:String){
+        let alert = UIAlertController(title: "에러", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "확인", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
